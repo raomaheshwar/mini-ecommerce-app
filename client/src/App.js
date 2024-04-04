937f9538b1e244fd9e850757e249eaaa00bb297c
raomaheshwar <raomaheshwar529@gmail.com>
2024-04-04 13:12:39 +0530

    Register Page and Login Page Created client side and some validation performed on it

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,19 @@ import React from 'react'
 import { Routes,Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import PageNotFound from './pages/PageNotFound'
+import Register from './pages/Auth/Register'
+import "react-toastify/dist/ReactToastify.css"
+import Login from './pages/Auth/Login';
+
 const App = () => {
   return (
     <>
     <Routes>
       <Route path='/' element={<HomePage/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/login' element={<Login/>}/>
       <Route path='*' element={<PageNotFound/>}/>
+     
     </Routes>
     </>
   )
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -3,7 +3,7 @@ import Layout from './../components/Layout/Layout'
 
 const HomePage = () => {
   return (
-    <Layout>
+    <Layout title={"Shop Now For Best Offers"}>
         <h1>HomePage</h1>
     </Layout>
   )
