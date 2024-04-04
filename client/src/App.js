7afce480756205da87cf3afdc7b77d359d5e531f
raomaheshwar <raomaheshwar529@gmail.com>
2024-04-04 11:34:30 +0530

    Client side Navbar, Footer, and Home page created

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import { Routes,Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import PageNotFound from './pages/PageNotFound'
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Routes>
+      <Route path='/' element={<HomePage/>}/>
+      <Route path='*' element={<PageNotFound/>}/>
+    </Routes>
+    </>
   )
 }
 
