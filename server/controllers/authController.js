caf3177e6ae5aa26a46208e3dcc3981c9001467f
raomaheshwar <raomaheshwar529@gmail.com>
2024-04-04 06:11:07 +0530

    Validation performed for user and admin access using JWT token and protected routes using middleware

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
-import { hashPassword } from "../helpers/authHelper.js";
+import { comparePassword, hashPassword } from "../helpers/authHelper.js";
 import userModels from "../models/userModels.js";
+import JWT from "jsonwebtoken";
 
 export const registerController = async(req, res)=>{
     try {
@@ -53,3 +54,58 @@ export const registerController = async(req, res)=>{
     }
 };
 
+//POST LOGIN
+export const loginController= async(req,res)=>{
+    try {
+        const {email, password}= req.body;
+        //validation
+        if(!email || !password){
+            return res.status(404).send({
+                success:false,
+                message:"Invalid email or password"
+            });
+        }
+        //check user
+        const user = await userModels.findOne({email})
+        if(!user){
+            return res.status(404).send({
+                success:false,
+                message:"Email is not registerd"
+            });
+        }
+        const match = await comparePassword(password, user.password)
+        if(!match){
+            return res.status(200).send({
+                success:false,
+                message:"Invalid Password"
+            });
+        }
+        //Token
+        const token= await JWT.sign({_id:user._id}, process.env.JWT_SECRET,{
+            expiresIn:"7d",
+        });
+        res.status(200).send({
+            success: true,
+            message:"Login Successfully",
+            user:{
+                name:user.name,
+                email:user.email,
+                phone:user.phone,
+                address:user.address,
+            },
+            token
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success:false,
+            message:"Error in login",
+            error
+        })
+    }
+}
+
+//test controller
+export const testController=()=>{
+   res.send("Protected Routes")
+}
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,11 +1,17 @@
 import express from "express";
-import {registerController}from "../controllers/authController.js";
-
+import {registerController,loginController, testController}from "../controllers/authController.js";
+import { isAdmin, requireSignIn } from "../middleware/authMiddleware.js";
 //router object
 const router = express.Router();
 
 //routing
 //Register || Method POST
 router.post("/register", registerController);
+
+//Login ||POST
+router.post("/login", loginController);
+
+//test routes
+router.get("/test",requireSignIn, isAdmin, testController);
 
 export default router;
